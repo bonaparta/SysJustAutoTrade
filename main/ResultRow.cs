76b1164fc51d8d70f50825954a3d1a4d1ff8268e
bonaparta <mingtsun17@gmail.com>
2017-09-25 22:03:51 +0800

    Rename variables

--- main/ResultRow.cs
+++ main/ResultRow.cs
@@ -8,35 +8,33 @@ namespace Comfup
 {
     class ResultRow
     {
-		ResultRow(string WaID, int ExpDays, string StID, string StNam, uint Quo, uint Lo, uint FiPa, uint Ref, uint UsaRa,
-                uint StQuo, uint StRef)
+		ResultRow(string WaID, int ExpDays, uint Quo, uint Ref, uint UsaRa, uint FiPa, string StID, string StNam,
+                uint SpotQuo, uint StRef)
 		{
             WarrantID = WaID;
             ExpiredDays = ExpDays;
-            TargetStockID = StID;
-            TargetStockName = StNam;
             Quote = Quo;
-            Low = Lo;
-            FixPercentReference = FiPa;
             ReferencePrice = Ref;
             UsageRatio = UsaRa;
-            TargetStockQuote = StQuo;
-            TargetStockReference = StRef;
+            FixPercentReference = FiPa;
+            SpotID = StID;
+            SpotName = StNam;
+            SpotQuote = SpotQuo;
+            SpotReference = StRef;
 
-            TargetStockLimitHigh = 0;
+            SpotLimitHigh = 0;
 		}
 		
         public string WarrantID { get; }
         public int ExpiredDays { get; }
-        public string TargetStockID { get; }
-        public string TargetStockName { get; }
+        public string SpotID { get; }
+        public string SpotName { get; }
         public float UnchangeGain { get; }
         public float LimitUpGain { get; }
         public float LimitDownGain { get; }
         // 報價 0.01 = 1
         public uint Quote { get; set; }
         public uint Volume { get; set; }
-        public uint Low { get; set; }
         public uint LimitLow { get; set; }
         public uint FixPercentReference { get; set; }
 		private uint ReferencePrice;
@@ -44,10 +42,10 @@ namespace Comfup
         public uint UsageRatio;
         public float leverage { get; }
         // 報價 0.01 = 1
-        public uint TargetStockQuote { get; }
-		private uint TargetStockLimitHigh { get; }
+        public uint SpotQuote { get; }
+		private uint SpotLimitHigh { get; }
 		private uint TargetStockLimitLow { get; }
-        private uint TargetStockReference { get; }
+        private uint SpotReference { get; }
         // Up / Down // rise / fall // higher / lower
         public float PLDay { get; }
     }
